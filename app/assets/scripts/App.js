306925a4a11b22c1b7838a5ad4ae61b69219a410
Thomas-Andre <hetlesaetherdev@gmail.com>
2021-09-13 12:34:53 +0200

    header html/css finished

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,3 +4,17 @@ import '../styles/styles.css';
 if (module.hot) {
     module.hot.accept();
 }
+
+function Person(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+    this.greet = function() {
+        console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor);
+    }
+}
+
+let john = new Person("John Doe", "blue");
+john.greet();
+
+let jane = new Person("Jane Smith", "green");
+jane.greet();
