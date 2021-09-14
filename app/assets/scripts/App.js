79de2e850ccb02e168393f303cb8a1f1f945a61e
Thomas-Andre <hetlesaetherdev@gmail.com>
2021-09-14 16:32:26 +0200

    header dragdown menu for mobile

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,10 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
 
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
     module.hot.accept();
 }
 
-function Person(fullName, favColor) {
-    this.name = fullName;
-    this.favoriteColor = favColor;
-    this.greet = function() {
-        console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor);
-    }
-}
-
-let john = new Person("John Doe", "blue");
-john.greet();
-
-let jane = new Person("Jane Smith", "green");
-jane.greet();
+mobileMenu
