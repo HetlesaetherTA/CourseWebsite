3d0f8cc690084b4577d18e34cfbd7a1e2f910b0e
Thomas-Andre <hetlesaetherdev@gmail.com>
2021-09-24 16:45:40 +0200

    Cloud update

--- could-functions/ppa.js
+++ could-functions/ppa.js
@@ -1,6 +1,29 @@
+// Netlify, postman
+
+let rank = "Super user or Owner";
+
 exports.handler = function(event, context, callback) {
+    const secretContent = `
+    <h3>Server Side Terminal Activated</h3>
+    <p>Permission rank: ${rank}</p>
+    `
+    let body;
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+        } else {
     callback(null, {
-        statusCode: 200,
-        body: "PPA activated"
-    })
+            statusCode: 401
+        })
+    }
+
 }
